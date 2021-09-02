79baf157429a50cb7d035d9d5ccd3e035d61573e
Shiva Shrestha <shiv.shres25@gmail.com>
2021-09-02 13:54:52 +0545

    adding list in test2

--- python_list.py
+++ python_list.py
@@ -1,3 +1,6 @@
 # list comprehension example
-list1 = ['code'*i for i in range(5) if i % 2 ==0]
-print(list1)
+list2 = ['codes'*i for i in range(5) if i % 2 ==0]
+list1 = ['coder'*i for i in range(5) if i % 2 ==0]
+list3 = ['code___'*i for i in range(5) if i % 2 ==0]
+
+print(list1, list2, list3)
