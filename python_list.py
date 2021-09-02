25d00e7054e6dee8f8de3a293bd27d642311d9de
Shiva Shrestha <shiv.shres25@gmail.com>
2021-09-02 14:17:30 +0545

    new change

--- python_list.py
+++ python_list.py
@@ -1,6 +1,7 @@
 # list comprehension example
-list2 = ['codes'*i for i in range(5) if i % 2 ==0]
-list1 = ['coder'*i for i in range(5) if i % 2 ==0]
-list3 = ['code___'*i for i in range(5) if i % 2 ==0]
+list1 = ['code'*i for i in range(5) if i % 2 ==0]
+list2 = ['python_'*i for i in range(5) if i % 2 ==0]
+list3 = ['code___'*i for i in range(56) if i % 4 ==0]
+
 
 print(list1, list2, list3)
